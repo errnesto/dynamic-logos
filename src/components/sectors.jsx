098b100224df48e9699a02f3c8e1c4a3c1540cc0
Manuel Reich <errnesto@me.com>
2017-05-31 21:16:01 +0200

    Move more graph parameters into props

--- src/components/sectors.jsx
+++ src/components/sectors.jsx
@@ -3,25 +3,31 @@ import { h, Component } from 'preact'
 import Sector from 'paths-js/sector'
 import Chroma from 'chroma-js'
 
+const range = ([ from, to ]) => {
+  let range = Array(to - from + 1).fill()
+  return range.map((_, i) => from + i)
+}
+
 export default class Sectors extends Component {
   static TWO_PI = 2 * Math.PI
-  static colorScale = Chroma.scale([
-    'FFFFFF',
-    'FEF5E2',
-    'FED89A',
-    'FEC349',
-    '86D0CB',
-    '38B7A8',
-    '828BBF',
-    '46558B',
-    '29255C'
-  ])
 
   constructor (props) {
     super()
+    this.colorScale = Chroma.scale([
+      'FFFFFF',
+      'FEF5E2',
+      'FED89A',
+      'FEC349',
+      '86D0CB',
+      '38B7A8',
+      '828BBF',
+      '46558B',
+      '29255C'
+    ]).domain(range(props.valueRange))
+
     // add some offset to values based on index
     // so they dont move all at the same time
-    this.state.values = props.values.map((value, index) => value + index * 0.3)
+    this.state.values = props.values.map((value, index) => value - 1 + index * 0.3)
   }
 
   componentDidMount () {
@@ -34,10 +40,7 @@ export default class Sectors extends Component {
     const minValue = this.props.valueRange[0]
     const maxValue = this.props.valueRange[1]
     const animationOffset = this.props.animationOffset
-    let diff = (timestamp - this.lastTimestamp || 0) / SPEED
-    // When in the background browsers won't execude requestAnimationFrame
-    // so diff can get pretty big before the animation direction changes
-    if (diff > 1) diff = 1
+    const diff = (timestamp - this.lastTimestamp || 0) / SPEED
     this.lastTimestamp = timestamp
 
     const values = this.state.values.map((value, index) => {
@@ -60,20 +63,28 @@ export default class Sectors extends Component {
     this.setState({ values }, () => { window.requestAnimationFrame(this.animate) })
   }
 
-  render ({ center, valueRange }, { values }) {
-    const parts = values.map((value, index) => ({
+  render ({ center, values, valueRange }, { values: animatedValues }) {
+    const parts = animatedValues.map((value, index) => ({
       path: Sector({
-        center: this.props.center,
+        center: center,
         r: 0,
         R: value,
-        start: Sectors.TWO_PI * index / values.length,
-        end: Sectors.TWO_PI * (index + 1) / values.length
+        start: Sectors.TWO_PI * index / animatedValues.length,
+        end: Sectors.TWO_PI * (index + 1) / animatedValues.length
       }).path.print(),
-      color: Sectors.colorScale(value / valueRange[1])
+      color: this.colorScale(value)
     }))
 
     return <svg width='500' height='500' viewBox='0 0 30 30'>
-      {parts.map(({ path, color }) =>
+      { range(valueRange).map(value =>
+        <circle cx={center[0]} cy={center[1]}
+          r={value}
+          fill='none'
+          stroke-width='0.06'
+          stroke={this.colorScale(value)} />
+      )}
+
+      { parts.map(({ path, color }) =>
         <path d={path} fill={color} stroke-width='0.06' stroke='white' />
       )}
     </svg>
