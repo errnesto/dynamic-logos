e0d0c425758a88ab02878f2dbbdd2678dcb28fa1
Manuel Reich <errnesto@me.com>
2017-06-01 11:27:28 +0200

    Add new axis

--- src/store.js
+++ src/store.js
@@ -6,7 +6,8 @@ const initialState = {
     wording: { name: 'Sprache', value: 2 },
     images: { name: 'Bildwelt', value: 6 },
     form: { name: 'Form', value: 8 },
-    font: { name: 'Typo', value: 7 }
+    font: { name: 'Typo', value: 7 },
+    orientation: { name: 'Ausrichtung', value: 4 }
   }
 }
 const actions = {
